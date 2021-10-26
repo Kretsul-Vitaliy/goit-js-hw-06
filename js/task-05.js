85915d85898546504cc08190a036bc7b69d65c50
Kretsul-Vitaliy <kreculvitalii@gmail.com>
2021-10-26 10:26:07 +0300

    fix task 5,7

--- js/task-05.js
+++ js/task-05.js
@@ -2,9 +2,9 @@ const input = document.querySelector('input#name-input');
 const output = document.querySelector('span#name-output');
 const handleInput = () => {
     const inputName = input.value.trim();
-    if (inputName !== ' ') { //если поставить без пробела будет ошибка при стирании текста в input
+    if (inputName !== '') {
         return (output.textContent = inputName);
     }
-    return (output.textContent = Anonymous);
+    return (output.textContent = 'Anonymous');
 };
 input.addEventListener('input', handleInput);
--- js/task-07.js
+++ js/task-07.js
@@ -1,5 +1,6 @@
 const range = document.querySelector('#font-size-control');
 const spanText = document.querySelector('#text');
+range.value = Number.parseInt(window.getComputedStyle(spanText).fontSize);
 const handleInput = (event) => {
     spanText.style.fontSize = `${event.currentTarget.value}px`;
 }
